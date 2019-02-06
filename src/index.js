bf9ea871c6e0dba43371e17fa82f150ebda2e85c
Rafał Rudol <rrrrudol@gmail.com>
2019-02-06 19:10:30 +0100

    #1 JSDoc documentation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import * as Cookies from "js-cookie";
 
+/**
+ * useCookie - React Hook for Cookies based on js-cookie
+ * @param {string} key Cookie name
+ * @param {Object|string} [initialValue]  Value will be assign if cookie doesn't exist.
+ * @returns {Array} Returns cookie value, and the function to update it.
+ */
 export function useCookie(key, initialValue) {
   const [item, setInnerValue] = useState(() => {
     return Cookies.get(key) || initialValue;
   });
 
+  /**
+   * Set value of cookie
+   * @param {Object|string} value 
+   * @param {Cookies.CookieAttributes} [options]
+   */
   const setValue = (value, options) => {
     setInnerValue(value);
     Cookies.set(key, value, options);
